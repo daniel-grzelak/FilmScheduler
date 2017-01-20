ffcf7cbc496ef3d479e8bef0fc6d1f4cd9601391
Daniel Grzelak <daniel.grzelak78@gmail.com>
2017-01-20 21:04:32 +0000

    1.0

---
+++ Swieta2_Kino/src/ReadFilmFile.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ReadFilmFile implements IFilm{
+
+	private Set<Film> set = new HashSet<>();
+	private final static String sciezka = "D://OneDrive/Programowanie/Pliki tekstowe/";
+	
+	public ReadFilmFile(String film) {
+		File plik = new File(sciezka + film + ".txt");
+		try(Scanner sc = new Scanner(plik)) {
+			
+			while(sc.hasNextLine()) {
+				String[] tab = sc.nextLine().split(" - ");
+				set.add(new Film(tab[0], tab[1], tab[2]));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public Set<Film> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Film> set) {
+		this.set = set;
+	}
+
+	@Override
+	public Film readFilm() {
+		Random rng = new Random();
+		List<Film> lista = new ArrayList<Film>(set);
+		return lista.get(rng.nextInt(lista.size()));
+	}	
+	
+	
+}
---
+++ Swieta2_Kino/src/ReadFilmRandom.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class ReadFilmRandom implements IFilm{
+
+	private final static String[] TITLES = {"CYTRYNDOR","Heheszek", "WHIPPED CREAM"};
+	private final static String[] GENRES = {"comedy", "HORROR", "thriller"};
+	private final static String[] DIRECTORS = {"Mr Mietek", "Kurt Cobain", "Cup of TeA"};
+	
+	@Override
+	public Film readFilm() {
+		Random rng = new Random();
+
+		
+		return new Film(TITLES[rng.nextInt(TITLES.length)], GENRES[rng.nextInt(GENRES.length)], DIRECTORS[rng.nextInt(DIRECTORS.length)]);
+		
+	}
+}
